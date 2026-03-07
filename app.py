74cfb5984390172e57f3ad1cb21c7daab7ebd2a7
PratikNick <161124255+Xenration@users.noreply.github.com>
2026-03-07 19:06:29 +0530

    Update import for PromptTemplate from langchain_core

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import feedparser
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationalRetrievalChain
